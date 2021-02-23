aa6fb13ee11daf1f3e456f212982effc0d2fc4e8
Brad Castellan <bcastell@buffalo.edu>
2021-02-23 17:18:30 -0500

    Finished tutorial 1

--- server/index.js
+++ server/index.js
@@ -2,23 +2,33 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 
-const clientPath = path.join(__dirname, '../client');
-const staticPath = path.join(clientPath, '/static');
-const viewsPath = path.join(clientPath, '/views');
+const clientPath = path.join(__dirname, '../client/');
+const staticPath = path.join(clientPath, '/static/');
+const viewsPath = path.join(clientPath, '/views/');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
 
+var x =0;
+const counter = function(req, res, next){
+    x++;
+    console.log(x);
+    next();
+}
+
+//app.use(counter);
+app.use(express.static(staticPath));
+
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/gameplay', function (req, res) {
-    res.render('gameplay');
+app.get('/gameplay', counter, function (req, res) {
+    res.render('gameplay', {count: x});
 });
 
-app.use(express.static(staticPath));
+
 
 app.listen(2000);
