e531c0ef13b96d2ca66aad62ae14f676ed7cceeb
Brad Castellan <bcastell@buffalo.edu>
2021-02-24 14:29:49 -0500

    Error at npm install express-session

--- server/index.js
+++ server/index.js
@@ -1,34 +1,56 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
+//Navagation
 const clientPath = path.join(__dirname, '../client/');
 const staticPath = path.join(clientPath, '/static/');
 const viewsPath = path.join(clientPath, '/views/');
 
+//Basic Server
 const app = express();
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    name: 'bullet',
+    secret: 'easierthanitlooks',
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        maxAge: 1000*60*60*24*3,
+    }
+}))
 
+app.listen(2000);
+
+//Setting Views
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
 
+//Visitor Counter
 var x =0;
 const counter = function(req, res, next){
     x++;
     console.log(x);
     next();
 }
-
 //app.use(counter);
-app.use(express.static(staticPath));
 
-app.get('/', function (req, res) {
-    res.render('index');
+
+//Routes
+
+app.get('/', function(req, res) {
+    res.render('index', {nomen: req.session.username});
 });
 
 app.get('/gameplay', counter, function (req, res) {
     res.render('gameplay', {count: x});
 });
 
-
-
-app.listen(2000);
+app.post('/welcome', (req, res) => {
+    //console.log(req.body.visitorname);
+    req.session.username=req.body.visitorname;
+    res.redirect('/');
+});
