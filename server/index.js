5b4df1f3662abbabc0aff0f403072e2584cdbedb
Remisleep <ardent1798@gmail.com>
2021-04-09 21:09:10 -0400

    more dumb problems

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const mongoose  = require('mongoose');
-const { BlogPost } = require('./models.js');
+const {BlogPost} = require('./models.js');
 
 //Navagation
 const clientPath = path.join(__dirname, '../client/');
@@ -53,11 +53,39 @@ app.get('/gameplay', function (req, res) {
 });
 
 app.get('/scoreblog/', (req, res) => {
-    res.render('scoreblog', {data: req.session});
+    var posts = BlogPost.find({}, (error, result) => {
+        if(error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+        console.log(posts);
+        res.render('scoreblog', {data: req.session, postset: result});
+    });
 });
 
-app.get('/scoreblog/writing', (req, res) => {
+app.get('/scoreblog/writing', async (req, res) => {
     res.render('writing', {data: req.session, draft: {}});
+});
+
+app.get('/blog/:id', async (req,res) => {
+    console.log("step 1");
+    var searchID = '';
+    console.log("step 2");
+    BlogPost.findById({_id: searchID}, (error, result) => {
+        if(error) {
+            console.log("step 3: error");
+            res.redirect('/scoreblog/');
+        }
+        else if(!result) {
+            console.log("step 3: not found");
+            res.redirect('/scoreblog/');
+        }
+        else {
+            console.log("step 3: good");
+            res.render('entry', {data: req.session, entry: result});
+            console.log("step 4");
+        }
+    })
 });
 
 app.post('/scoreblog/writepost', async (req, res) => {
@@ -71,4 +99,14 @@ app.post('/scoreblog/writepost', async (req, res) => {
 app.post('/welcome', (req, res) => {
     req.session.username=req.body.nombre;
     res.send('SUCCESS');
+});
+
+app.put('/blog/update', (req, res)=> {
+    console.log(req);
+    res.redirect('/scoreblog/')
+});
+
+app.delete('/blog/delete', (req, res)=> {
+    console.log(req);
+    res.redirect('/scoreblog/')
 });
